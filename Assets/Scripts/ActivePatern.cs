e1b8b6089080d97a12a362edc795edb37c2dc235
JuhTyg010 <sadovsky11143@gmail.com>
2023-05-01 08:59:53 +0200

    Create pattern mechanic

---
+++ Assets/Scripts/ActivePatern.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivePatern : MonoBehaviour
+{
+    //TODO: active patern will show ships to generate with some ships recount the optimal pattern and ship count
+}
---
+++ Assets/Scripts/Paterns.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paterns : MonoBehaviour
+{
+    void Patern1()
+    {
+        //TODO: go dawn left and right
+    }
+
+    void Patern2()
+    {
+        //TODO: something else than first one
+    }
+}
