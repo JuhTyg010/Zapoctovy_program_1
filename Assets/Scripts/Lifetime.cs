477a921ddb895dad68bf5ca0bfde0d1a7ccc2a83
JuhTyg010 <sadovsky11143@gmail.com>
2023-07-12 11:12:34 +0200

    Create script to destroy object by time

---
+++ Assets/Scripts/Lifetime.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Lifetime : MonoBehaviour
+{
+    [SerializeField] private float lifeTime = 1;
+    
+    private float _lifeTimer;
+    void Start()
+    {
+        _lifeTimer = lifeTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_lifeTimer <= 0)
+        {
+            Destroy(gameObject);
+        }
+        _lifeTimer -= Time.deltaTime;
+    }
+}
