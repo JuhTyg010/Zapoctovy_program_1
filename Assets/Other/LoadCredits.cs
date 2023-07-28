a1005c2a6b779e42c741e103c49a3631fed7a8b1
JuhTyg010 <sadovsky11143@gmail.com>
2023-07-28 15:10:22 +0200

    Create credits panel

---
+++ Assets/Other/LoadCredits.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LoadCredits : MonoBehaviour
+{
+    [SerializeField] private GameObject credits;
+    
+    public void LoadAllCredits()
+    {
+        //TODO: Load Credits
+    }
+}
